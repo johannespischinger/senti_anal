289f0bd7d2061afec34e6f6e05af1fe9341513e9
Michael Feil <michaelfeil@example.com>
2022-01-13 16:29:13 +0100

    gh-pages cleanup

--- opensentiment/utils.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-
-def get_project_root() -> Path:
-    """return Path to the project directory, top folder of opensentiment"""
-    return Path(__file__).parent.parent
--- tests/models/test_bert_model.py
+++
@@ -1,8 +0,0 @@
-from opensentiment.models.bert_model import SentimentClassifier
-
-
-class TestSentimentClassifier:
-    def test_loadmodel(self):
-        model = SentimentClassifier()
-        # test if no error when model loaded
-        assert model.parameters()
