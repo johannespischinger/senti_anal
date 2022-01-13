0219deacf3ddc79c375c061609ddb8aca227eec9
maximilian.frantzen@unternehmertum.de <maximilian.frantzen@unternehmertum.de>
2022-01-13 11:55:32 +0100

    adds updates dummy samples

--- opensentiment/data/utils.py
+++ opensentiment/data/utils.py
@@ -4,48 +4,45 @@ import os
 import logging
 import transformers
 from opensentiment.data.dataset import AmazonPolarity
+from typing import Tuple
+import numpy as np
 
 logger = logging.getLogger(__name__)
 ROOT_PATH = Path(__file__).resolve().parents[2]
 
 
-def truncate_dataset(path):
-    pass
+def truncate_dataset(file, steps: int = 1000) -> Tuple[np.array, np.array]:
+    train_set = torch.load(os.path.join(ROOT_PATH, f"data/processed/{file}"))
+    return train_set.sample[::steps], train_set.target[:steps]
 
 
 def create_sample_dataset(
         max_len: int = 128,
         tokenizer_name: str = "bert-base-cased",
 ) -> None:
-    train_set = torch.load(os.path.join(ROOT_PATH, "data/processed/train_dataset.pt"))
-    train_small_data = train_set.sample[::1000]
-    train_small_label = train_set.target[:1000]
-    val_set = torch.load(os.path.join(ROOT_PATH, "data/processed/val_dataset.pt"))
-    val_small_data = val_set.sample[::1000]
-    val_small_label = val_set.target[::1000]
-    test_set = torch.load(os.path.join(ROOT_PATH, "data/processed/test_dataset.pt"))
-    test_small_data = test_set.sample[::1000]
-    test_small_label = test_set.target[::1000]
+    train = truncate_dataset("train_dataset.pt")
+    val = truncate_dataset("val_dataset.pt")
+    test = truncate_dataset("test_dataset.pt")
 
     logger.info("Create training dataset...")
     tokenizer = transformers.BertTokenizer.from_pretrained(tokenizer_name)
     train_data = AmazonPolarity(
-        sample=train_small_data,
-        target=train_small_label,
+        sample=train[0],
+        target=train[1],
         tokenizer=tokenizer,
         max_len=max_len,
     )
     logger.info("Create validation dataset...")
     val_data = AmazonPolarity(
-        sample=val_small_data,
-        target=val_small_label,
+        sample=val[0],
+        target=val[1],
         tokenizer=tokenizer,
         max_len=max_len,
     )
     logger.info("Create test dataset...")
     test_data = AmazonPolarity(
-        sample=test_small_data,
-        target=test_small_label,
+        sample=test[0],
+        target=test[1],
         tokenizer=tokenizer,
         max_len=max_len,
     )
