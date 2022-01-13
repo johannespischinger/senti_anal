fc26567259fa69edc4018cd23ffa35f21a60cec2
Michael Feil <michaelfeil@example.com>
2022-01-13 20:32:25 +0100

    fix tests

--- tests/data/test_dataset.py
+++ tests/data/test_dataset.py
@@ -2,6 +2,7 @@ from opensentiment.data.dataset import AmazonPolarity
 import transformers
 from pathlib import Path
 import torch
+from torch.utils.data.dataloader import DataLoader
 import os
 import pytest
 from opensentiment.utils import get_project_root
@@ -18,16 +19,8 @@ class TestAmazonPolarity:
             os.path.join(get_project_root(), "tests/dummy_dataset/test_dataset.pt")
         )
         dataset = AmazonPolarity(samples.sample[:2], samples.target[:2], tokenizer, 128)
-
-        assert (
-            hasattr(dataset, "review")
-            and hasattr(dataset, "input_id")
-            and hasattr(dataset, "attention_mask")
-            and hasattr(dataset, "target")
-        ), f"missing arributes of dataset {dataset.__dict__}"
-        assert dataset.review, "Missing samples in dataset"
-        assert dataset.input_id, "Missing input ids in dataset"
-        assert (
-            dataset.attention_mask
-        ), "Missing attention mask for BERT model in dataset"
-        assert dataset.target, "Missing labels in dataset"
+        dataloader_set = next(iter(DataLoader(dataset)))
+        assert all(
+            i in dataloader_set
+            for i in ["input_id", "review", "attention_mask", "target"]
+        ), f"missing arributes of dataset {dataloader_set}"
