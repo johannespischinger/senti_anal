cfe8fd9c81ad8d1e508102de0acf2c7fd14637d0
maximilian.frantzen@unternehmertum.de <maximilian.frantzen@unternehmertum.de>
2022-01-13 19:04:45 +0100

    updates git checkout tag for data version

--- run_docker.sh
+++ run_docker.sh
@@ -5,13 +5,13 @@ echo "wandb_key_api=$2"
 echo "git_tag=$3"
 
 git pull
-if [ "$3" -ne "" ]
+if [ "$3" != "" ]
 then
   echo "checkout {$3}"
-  git checkout "$3"
+  git checkout "$3" data.dvc
 else
   echo "checkout v1.0"
-  git checkout v1.0
+  git checkout v1.0 data.dvc
 fi
 
 dvc pull
