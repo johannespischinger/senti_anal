55c906613bb46387697799cc091805d462bdcdf1
maximilian.frantzen@unternehmertum.de <maximilian.frantzen@unternehmertum.de>
2022-01-13 18:25:53 +0100

    adds git checkout tag for data version

--- run_docker.sh
+++ run_docker.sh
@@ -2,6 +2,17 @@
 
 echo "experiments=$1"
 echo "wandb_key_api=$2"
+echo "git_tag=$3"
+
+git pull
+if [ "$3" -ne "" ]
+then
+  echo "checkout {$3}"
+  git checkout "$3"
+else
+  echo "checkout v1.0"
+  git checkout v1.0
+fi
 
 dvc pull
 python -u opensentiment/models/train_model.py "experiments=$1" "wandb_key_api=$2"
