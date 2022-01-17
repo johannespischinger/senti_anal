b44dbb1b13c6279eee08e901f4388fb72940e784
Michael Feil <michaelfeil@example.com>
2022-01-17 17:14:42 +0100

    rerun tests

--- tests/models/test_train_model_pl.py
+++ tests/models/test_train_model_pl.py
@@ -11,11 +11,11 @@ from opensentiment.models import train_model_pl
 from opensentiment.utils import get_project_root, return_omegaconf_modified
 
 
-@pytest.mark.long  # 8 min
 @pytest.mark.parametrize(
     "config",
     [(return_omegaconf_modified({"train": {"pl_trainer": {"max_steps": 800}}}))],
 )
+@pytest.mark.long  # 8 min
 def test_train(
     config: omegaconf.OmegaConf,
 ):
@@ -34,6 +34,6 @@ def test_train2(
     config: omegaconf.OmegaConf,
 ):
     hydra.core.global_hydra.GlobalHydra.instance().clear()
-    hydra_dir = os.path.join(get_project_root(), ".cache", "Hydratest")
+    hydra_dir = os.path.join(get_project_root(), ".cache", "Hydratest2")
     os.makedirs(hydra_dir, exist_ok=True)
     train_model_pl.train(config, hydra_dir, use_val_test=False)
