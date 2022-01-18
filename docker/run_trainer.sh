4764f95522152ab5a8e97bb1f744cd1d3232b4df
maximilian.frantzen@unternehmertum.de <maximilian.frantzen@unternehmertum.de>
2022-01-18 18:44:12 +0100

    adds docker files

---
+++ docker/run_trainer.sh
@@ -0,0 +1,24 @@
+#! /bash/sh
+
+echo "experiments=$a"
+echo "wandb_key_api=$2"
+echo "git_tag=$3"
+echo "job_dir_gs=$4"
+
+#git pull
+if [ "$3" != "" ]
+then
+  echo "git checkout {$3}"
+  git checkout "$3" data.dvc
+else
+  echo "git checkout v1.0"
+  git checkout v1.0 data.dvc
+fi
+dvc pull
+
+if [ "$a" != "" ]
+then
+  export a=exp0
+fi
+echo "Using $a experiment setting!"
+python -u opensentiment/models/train_model.py "experiments=$a" "wandb_key_api=$2" "job_dir_gs=$4"
