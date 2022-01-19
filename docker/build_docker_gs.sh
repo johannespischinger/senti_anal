aff2dd669434644b52dce1009259a39aa451e0dd
maximilian.frantzen@unternehmertum.de <maximilian.frantzen@unternehmertum.de>
2022-01-19 10:18:01 +0100

    updates docker files

--- docker/build_docker_gs.sh
+++ docker/build_docker_gs.sh
@@ -5,5 +5,6 @@ export IMAGE_REPO_NAME=bert_training
 export IMAGE_TAG=bert_training_cpu
 export IMAGE_URI=gcr.io/"$PROJECT_ID"/"$IMAGE_REPO_NAME":"$IMAGE_TAG"
 
-docker build -f trainer_pl.dockerfile -t "$IMAGE_URI" ./
+cd ..
+docker build --no-cache -f docker/trainer_pl.dockerfile -t "$IMAGE_URI" ./
 # docker push "$IMAGE_URI"
