2b0e1ab83f48c8c2f2efa7345bdc8ac8f3c46202
maximilian.frantzen@unternehmertum.de <maximilian.frantzen@unternehmertum.de>
2022-01-20 16:17:58 +0100

    updates cloud config

--- request.py
+++ request.py
@@ -1,9 +1,13 @@
 import time
 import requests
+import json
 
 url = 'https://us-central1-sensi-anal.cloudfunctions.net/senti_anal_2'
 payload = {'message': 'this is bad'}
 
+newHeaders = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+headers = newHeaders
 for _ in range(1000):
-    r = requests.post(url, data=payload)
+    #r = requests.post(url, data=payload, headers=newHeaders)
+    r = requests.post(url, json=payload, headers=newHeaders)
     print(r)
