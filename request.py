32ef86b0324fab04bbfd582c696aeda0d5f362f9
maximilian.frantzen@unternehmertum.de <maximilian.frantzen@unternehmertum.de>
2022-01-20 16:02:13 +0100

    updates cloud config

--- request.py
+++ request.py
@@ -5,5 +5,5 @@ url = 'https://us-central1-sensi-anal.cloudfunctions.net/senti_anal_2'
 payload = {'message': 'this is bad'}
 
 for _ in range(1000):
-    r = requests.get(url, params=payload)
+    r = requests.post(url, data=payload)
     print(r)
