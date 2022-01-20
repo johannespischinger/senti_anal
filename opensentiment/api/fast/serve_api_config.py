7047de526cb4fe7d795ec9038f5ba56165686a79
Michael Feil <michaelfeil@example.com>
2022-01-20 11:58:51 +0100

    add new model to dvc, fastapi

--- opensentiment/api/fast/serve_api_config.py
+++ opensentiment/api/fast/serve_api_config.py
@@ -1,12 +1,18 @@
-from opensentiment.utils import get_project_root
+from opensentiment.utils import get_project_root, paths_to_file_ext
 import os
 
+
+model_path_desired = os.path.join(
+    get_project_root(),
+    "model_store",
+    "distilbert-finetuned-2022-01-20",
+    "BERT",
+    "m5whu2bt",
+    "checkpoints",
+    "epoch=4-step=494.ckpt",
+)
+
 CONFIG = {
-    "MODEL_CPTH_PATH": os.path.join(
-        get_project_root(),
-        "model_store",
-        "pretrained-distilbert-2022-01-19",
-        "BERT",
-        "epoch=1-step=14.ckpt",
-    )
+    "MODEL_CPTH_PATH": model_path_desired,
+    "FALLBACK_MODEL": ["model_store", "ckpt"],
 }
