871e0ba34622afe6d390196fe1fa974b9aa931b1
Michael Feil <michaelfeil@example.com>
2022-01-21 10:13:41 +0100

    update prediction model to use gpu

--- opensentiment/models/predict_model_pl.py
+++ opensentiment/models/predict_model_pl.py
@@ -6,13 +6,24 @@ import hydra
 
 
 class Prediction:
-    def __init__(self, path_to_checkpoint: str = ""):
+    def __init__(self, path_to_checkpoint: str = "", preferred_device: str = "cuda"):
+        """
+        prediction module
 
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.loaded_model = SentimentClassifierPL.load_from_checkpoint(
-            path_to_checkpoint, map_location=device, load_pretrain_weights=False
+        params
+            path_to_checkpoint: os.path to model.ckpt
+            preferred_device: "cuda" or "cpu",
+                default is "cuda" if torch.cuda.is_available() else "cpu"
+                "cpu" uses always cpu
+        """
+        self.device = torch.device(
+            preferred_device if torch.cuda.is_available() else "cpu"
         )
-        self.loaded_model.eval()
+        self.cpu = torch.device("cpu")
+        self.loaded_model = SentimentClassifierPL.load_from_checkpoint(
+            path_to_checkpoint, map_location=self.device, load_pretrain_weights=False
+        )
+        self.loaded_model.eval().to(self.device)
 
         self.data_module: pl.LightningDataModule = hydra.utils.instantiate(
             self.loaded_model.data["datamodule"], _recursive_=False
@@ -20,12 +31,13 @@ class Prediction:
 
     def predict(self, x: dict):
         """
+        return prediction
+
         x: dict
             x['content']: list of strings
-            x["label"]: Any / list of labels, currently not used, dummy required
             x["title"]: list of strings, currently not used
         """
-        # x as dict with x['content'] with List of inputs & x['label']=None
+        x.update({"label": None})
         features = self.data_module.convert_to_features(x)
         input_ids, attention_masks = (
             features["input_ids"],
@@ -34,14 +46,15 @@ class Prediction:
         dict_class = {0: "Negativ", 1: "Positive"}
         with torch.no_grad():
             out = self.loaded_model.forward(
-                torch.LongTensor(input_ids), torch.FloatTensor(attention_masks)
+                torch.LongTensor(input_ids).to(self.device),
+                torch.FloatTensor(attention_masks).to(self.device),
             )[0]
-        out_p = torch.nn.functional.softmax(out, dim=1)
+        out = torch.nn.functional.softmax(out, dim=1).to(self.cpu)
         # out_p = torch.exp(out) # softmax
-        _, sentiment = torch.max(out_p, dim=1)
+        _, sentiment = torch.max(out, dim=1)
 
         return [
-            (sentence, dict_class[int(x)], out_p[count].tolist())
+            (sentence, dict_class[int(x)], out[count].tolist())
             for count, (sentence, x) in enumerate(zip(x["content"], sentiment))
         ]
 
@@ -50,11 +63,10 @@ if __name__ == "__main__":
 
     prediction_model = Prediction(
         str(get_project_root())
-        + "/.cache/2022-01-19/17-49-18/BERT/14yg4vjs/checkpoints/epoch=1-step=14.ckpt",
+        + "/model_store/distilbert-finetuned-2022-01-20/BERT/m5whu2bt/checkpoints/epoch=4-step=494.ckpt",
     )
     x = {
-        "content": ["This is horrible", "not recommended", "This is perfect bitch", ""],
-        "label": None,
+        "content": ["This is horrible", "not recommended", "This is perfect bitch", ""]
     }
     answer = prediction_model.predict(x)
     x = {"content": ["This is very good"], "label": None}
